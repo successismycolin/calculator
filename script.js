047387f64f516b132e9da83e0531b26eee12d5e7
successismycolin <spumelelebuthelezi@gmail.com>
2026-07-04 04:07:57 +0200

    Add initial implementation of calculator logic in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// 1. Select elements
+// 2. Input validation
+    // If current character is an operator and the next character chosen is also an operator then the most recent
+    // operator will replace the old one in the display field.
+// 3. Add character to display field
+// 4. Addition
+// 5. Subtraction
+// 6. Division
+// 7. Multiplication
+// 8. Operation execution
+// 9. Continuous operations
