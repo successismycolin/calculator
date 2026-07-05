b522102366110732ae685bd0131a7326080adde2
successismycolin <spumelelebuthelezi@gmail.com>
2026-07-05 02:29:31 +0200

    Remove individual button element selections

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 // 1. Select elements
 const inputbox = document.getElementById("textbox");
-const btn1 = document.getElementById("1");
-const btn2 = document.getElementById("2");
-const btn3 = document.getElementById("3");
-const btn4 = document.getElementById("4");
-const btn5 = document.getElementById("5");
-const btn6 = document.getElementById("6");
-const btn7 = document.getElementById("7");
-const btn8 = document.getElementById("8");
-const btn9 = document.getElementById("9");
-const btn0 = document.getElementById("0");
-const btnPlus = document.getElementById("+");
-const btnMinus = document.getElementById("-");
-const btnDivide = document.getElementById("/");
-const btnMultiply = document.getElementById("x");
-const btnEquals = document.getElementById("=");
 
 const operators = ["+", "-", "*", "/"]
 
